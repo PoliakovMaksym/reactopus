57454acb3a6c96d3541ac830a30e7acaf609190a
Poliakov Maksym <polmax09@gmail.com>
2019-02-22 07:55:07 +0200

    Add Root module with placeholder logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { RootModule } from './modules/Root';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<RootModule />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/modules/Root/RootModule.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class RootModule extends React.Component {
+  // RENDER
+
+  render() {
+    return <div>Root module render results</div>;
+  }
+}
